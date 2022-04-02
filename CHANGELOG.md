0fd798a46d2c0f286824e541eba4dd0560e79a81
zhangcong <zhangcong@9linktech.com>
2022-04-02 14:14:57 +0800

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+<a name=""></a>
+##  (2022-04-02)
+
+* initial commit ([6aff2f6](https://gitlab.doex.io/zhang/vite-ts-starter/commit/6aff2f6))
+
+
+
---
+++ vite-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+/// <reference types="vue/ref-macros" />
+/// <reference types="element-plus/global" />
+
+interface ImportMetaEnv {
+  VITE_REPORTER_PROJECT_ID: string
+}
